6fe896333824224f4ca15be6be2ac47416c20c5e
TungVu <tungvuduc2805@gmail.com>
2021-07-01 16:37:58 +0700

    requests data from the given URLRequest

--- TheMovieDB/TheMovieDBTests/API/URLSessionHTTPClientTests.swift
+++ TheMovieDB/TheMovieDBTests/API/URLSessionHTTPClientTests.swift
@@ -6,11 +6,63 @@
 //
 
 import XCTest
+import TheMovieDB
+
+class URLSessionHTTPClient {
+    private let session: URLSession
+    
+    init(session: URLSession) {
+        self.session = session
+    }
+    
+    func dispatch(request: URLRequest) {
+        session.dataTask(with: request)
+            .resume()
+    }
+}
 
 class URLSessionHTTPClientTests: XCTestCase {
     
+    override func setUp() {
+        super.setUp()
+        URLProtocolStub.removeStub()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+        URLProtocolStub.removeStub()
+    }
+    
+    func test_dispatch_requestsfromURL() {
+        let configuration = URLSessionConfiguration.ephemeral
+        configuration.protocolClasses = [URLProtocolStub.self]
+        let session = URLSession(configuration: configuration)
+        let request = makeRequestFrom()
+        let sut = URLSessionHTTPClient(session: session)
+        
+        let exp = expectation(description: "wait for completion")
+        URLProtocolStub.observeRequests { receivedRequest in
+            XCTAssertEqual(request, receivedRequest)
+            exp.fulfill()
+        }
+        
+        sut.dispatch(request: request)
+        
+        wait(for: [exp], timeout: 0.1)
+    }
     
     // MARK: - Helpers
+    private func makeRequestFrom(credential: Credential = .init(apiKey: "any"), url: URL = URL(string: "http://any-url.com")!, page: Int = 1) -> URLRequest {
+        var components = URLComponents(url: url, resolvingAgainstBaseURL: false)!
+        components.queryItems = [
+            URLQueryItem(name: "api_key", value: credential.apiKey),
+            URLQueryItem(name: "page", value: "\(page)")
+        ]
+        
+        return URLRequest(url: components.url!)
+    }
+
+    
     final class URLProtocolStub: URLProtocol {
         
         private struct Stub {
