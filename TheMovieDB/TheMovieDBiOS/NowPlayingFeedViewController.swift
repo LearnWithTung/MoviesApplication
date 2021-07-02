6bb24185f8f7fbb1eb56a05d123cb4749745343f
TungVu <tungvuduc2805@gmail.com>
2021-07-02 18:13:07 +0700

    extract `NowPlayingItemController` to its own file

--- TheMovieDB/TheMovieDBiOS/NowPlayingFeedViewController.swift
+++ TheMovieDB/TheMovieDBiOS/NowPlayingFeedViewController.swift
@@ -8,47 +8,6 @@
 import UIKit
 import TheMovieDB
 
-public final class NowPlayingItemController {
-    private let model: NowPlayingCard
-    private let imageLoader: MovieImageDataLoader
-    private var task: MovieImageDataTask?
-    
-    init(model: NowPlayingCard, imageLoader: MovieImageDataLoader) {
-        self.model = model
-        self.imageLoader = imageLoader
-    }
-    
-    public func view(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "NowPlayingCardFeedCell", for: indexPath) as! NowPlayingCardFeedCell
-        cell.imageView.isShimmering = true
-        cell.imageView.image = nil
-        task = imageLoader.load(from: makeURL(from: model.imagePath)) {[weak self, weak cell] result in
-            guard self?.task != nil else {
-                cell = nil
-                return
-            }
-            let image = (try? result.get()).flatMap(UIImage.init)
-            cell?.imageView.isShimmering = image == nil
-            cell?.imageView.image = image
-        }
-        
-        return cell
-    }
-    
-    private func makeURL(from path: String) -> URL {
-        return URL(string: "https://image.tmdb.org/t/p/w500/\(path)")!
-    }
-    
-    func preload() {
-        task = imageLoader.load(from: makeURL(from: model.imagePath)) { _ in }
-    }
-    
-    func cancelTask() {
-        task?.cancel()
-        task = nil
-    }
-}
-
 public final class NowPlayingFeedViewController: UICollectionViewController, UICollectionViewDataSourcePrefetching {
     private var refreshController: NowPlayingRefreshController?
     private var imageLoader: MovieImageDataLoader?
