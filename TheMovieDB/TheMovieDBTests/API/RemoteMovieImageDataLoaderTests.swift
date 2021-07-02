3bea53a106c9c38dd509e139a38cc78ac71e9988
TungVu <tungvuduc2805@gmail.com>
2021-07-02 21:31:44 +0700

    move `HTTPClientSpy` to a shared scope

--- TheMovieDB/TheMovieDBTests/API/RemoteMovieImageDataLoaderTests.swift
+++ TheMovieDB/TheMovieDBTests/API/RemoteMovieImageDataLoaderTests.swift
@@ -52,33 +52,4 @@ class RemoteMovieImageDataLoaderTests: XCTestCase {
     }
     
     // MARK: - Helpers
-    
-    private class HTTPClientSpy: HTTPClient {
-        
-        private var messages = [(request: URLRequest, completion: (HTTPClient.HTTPClientResult) -> Void)]()
-        
-        var requestedURLs: [URLRequest] {
-            return messages.map { $0.request }
-        }
-        
-        private struct Task: HTTPClientTask {
-            func cancel() {}
-        }
-        
-        func dispatch(request: URLRequest, completion: @escaping (HTTPClient.HTTPClientResult) -> Void)  -> HTTPClientTask {
-            messages.append((request, completion))
-            
-            return Task()
-        }
-        
-        func completeWithError(_ error: Error, at index: Int = 0) {
-            messages[index].completion(.failure(error))
-        }
-        
-        func completeWith(statusCode code: Int, data: Data = Data(), at index: Int = 0) {
-            let httpResponse = HTTPURLResponse(url: requestedURLs[index].url!, statusCode: code, httpVersion: nil, headerFields: nil)!
-            messages[index].completion(.success((data, httpResponse)))
-        }
-        
-    }
 }
