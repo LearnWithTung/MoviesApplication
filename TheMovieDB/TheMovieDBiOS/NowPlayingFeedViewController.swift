24922a3cbc20e23e5bcdf27be2206ae8e354fbcd
TungVu <tungvuduc2805@gmail.com>
2021-07-02 17:58:05 +0700

    move `NowPlayingRefreshController` to its own file

--- TheMovieDB/TheMovieDBiOS/NowPlayingFeedViewController.swift
+++ TheMovieDB/TheMovieDBiOS/NowPlayingFeedViewController.swift
@@ -8,34 +8,6 @@
 import UIKit
 import TheMovieDB
 
-public final class NowPlayingRefreshController: NSObject {
-    private let loader: NowPlayingLoader
-    
-    init(loader: NowPlayingLoader) {
-        self.loader = loader
-    }
-    
-    lazy var view: UIRefreshControl = {
-        let refreshControl = UIRefreshControl()
-        refreshControl.addTarget(self, action: #selector(refresh), for: .valueChanged)
-
-        return refreshControl
-    }()
-    
-    var onRefresh: ((NowPlayingFeed) -> Void)?
-    
-    @objc func refresh() {
-        view.beginRefreshing()
-        loader.load(query: .init(page: 1)) {[weak self] result in
-            self?.view.endRefreshing()
-            if let feed = try? result.get() {
-                self?.onRefresh?(feed)
-            }
-        }
-    }
-    
-}
-
 public final class NowPlayingFeedViewController: UICollectionViewController, UICollectionViewDataSourcePrefetching {
     private var refreshController: NowPlayingRefreshController?
     private var imageLoader: MovieImageDataLoader?
