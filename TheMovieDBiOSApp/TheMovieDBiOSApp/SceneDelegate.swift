922965bff7548b975eedb5290f1f91a761a9bc1a
TungVu <tungvuduc2805@gmail.com>
2021-07-02 18:51:36 +0700

    add `TheMovieDBiOSApp` project

---
+++ TheMovieDBiOSApp/TheMovieDBiOSApp/SceneDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  SceneDelegate.swift
+//  TheMovieDBiOSApp
+//
+//  Created by Tung Vu on 02/07/2021.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+}
+
