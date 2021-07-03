78d498bd1547227f245a2a3a9f80a6dfcb4ed04b
TungVu <tungvuduc2805@gmail.com>
2021-07-03 19:02:23 +0700

    compose feed view controller

--- TheMovieDBiOSApp/TheMovieDBiOSApp/Application/SceneDelegate.swift
+++ TheMovieDBiOSApp/TheMovieDBiOSApp/Application/SceneDelegate.swift
@@ -12,5 +12,35 @@ import TheMovieDBiOS
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
+    private lazy var navController = UINavigationController()
+
+    private lazy var baseURL = URL(string: "https://api.themoviedb.org/3/movie/now_playing")!
+    private lazy var credential = Credential(apiKey: "494d9fe55bdb97bc7ee0b57dfa80751b")
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+
+        let window = UIWindow(windowScene: windowScene)
+
+        navController.setViewControllers([makeNowPlayingScene()], animated: true)
+        window.rootViewController = navController
+
+        self.window = window
+        self.window?.makeKeyAndVisible()
+    }
+    
+    func makeNowPlayingScene() -> NowPlayingFeedViewController {
+        let client = URLSessionHTTPClient(session: .init(configuration: .ephemeral))
+        let authenticatedClient = AuthenticatedHTTPClientDecorator(decoratee: client, credential: credential)
+        
+        let loader = RemoteNowPlayingFeedLoader(url: baseURL, client: authenticatedClient)
+        let imageLoader = RemoteMovieImageDataLoader(client: client)
+        
+        let viewController = NowPlayingFeedComposer.viewControllerComposedWith(feedLoader: loader, imageLoader: imageLoader)
+        viewController.title = "Now Playing"
+        
+        return viewController
+    }
+
 }
 
