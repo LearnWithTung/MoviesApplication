12cfc00d275c6de3ba3234f88742ce05e291d07d
TungVu <tungvuduc2805@gmail.com>
2021-07-03 12:41:23 +0700

    Move `AuthenticatedHTTPClientDecorator` to production

--- TheMovieDBiOSApp/TheMovieDBiOSAppTests/AuthenticatedHTTPClientDecoratorTests.swift
+++ TheMovieDBiOSApp/TheMovieDBiOSAppTests/AuthenticatedHTTPClientDecoratorTests.swift
@@ -7,55 +7,7 @@
 
 import XCTest
 import TheMovieDB
-
-class AuthenticatedHTTPClientDecorator: HTTPClient {
-    private let decoratee: HTTPClient
-    private let credential: Credential
-    
-    init(decoratee: HTTPClient, credential: Credential) {
-        self.decoratee = decoratee
-        self.credential = credential
-    }
-    private struct DecoratedTask: HTTPClientTask {
-        private let decorateeTask: HTTPClientTask
-        
-        init(decorateeTask: HTTPClientTask) {
-            self.decorateeTask = decorateeTask
-        }
-        
-        func cancel() {
-            decorateeTask.cancel()
-        }
-    }
-    
-    func dispatch(request: URLRequest, completion: @escaping (HTTPClientResult) -> Void) -> HTTPClientTask {
-        let decorateeTask = decoratee.dispatch(request: makeRequest(from: request, credential: credential)) { [weak self] result in
-            guard self != nil else {return}
-            switch result {
-            case let .success((data, response)):
-                completion(.success((data, response)))
-            case let .failure(error):
-                completion(.failure(error))
-            }
-        }
-        return DecoratedTask(decorateeTask: decorateeTask)
-    }
-    
-    private func makeRequest(from original: URLRequest, credential: Credential) -> URLRequest {
-        guard let requestURL = original.url, var components = URLComponents(url: requestURL, resolvingAgainstBaseURL: false) else {return original}
-        let queriesItem = components.queryItems ?? []
-        let decoratedQuery = URLQueryItem(name: "api_key", value: credential.apiKey)
-        
-        components.queryItems = queriesItem + [decoratedQuery]
-        
-        guard let authenticatedRequestURL = components.url else { return original }
-
-        var signedRequest = original
-        signedRequest.url = authenticatedRequestURL
-        return signedRequest
-    }
-
-}
+import TheMovieDBiOSApp
 
 class AuthenticatedHTTPClientDecoratorTests: XCTestCase {
     
