a7eb820a698b8dd5dd907cb5853c4d55a19ffb70
TungVu <tungvuduc2805@gmail.com>
2021-07-04 14:42:52 +0700

    Move `NowPlayingItemAdapter` to its own file

--- TheMovieDB/TheMovieDBiOS/Composers/FeedViewAdapter.swift
+++ TheMovieDB/TheMovieDBiOS/Composers/FeedViewAdapter.swift
@@ -33,37 +33,3 @@ final class FeedViewAdapter: FeedView {
         }
     }
 }
-
-final class NowPlayingItemAdapter<View: ImageDataView, Image>: NowPlayingItemDelegate where View.Image == Image {
-    private let imageLoader: MovieImageDataLoader
-    var presenter: NowPlayingItemPresenter<View, Image>?
-    private let model: NowPlayingCard
-    private var task: MovieImageDataTask?
-    
-    init(imageLoader: MovieImageDataLoader, model: NowPlayingCard){
-        self.imageLoader = imageLoader
-        self.model = model
-    }
-    
-    func didRequestLoadImageData() {
-        presenter?.didStartLoadingImageData()
-        task = imageLoader.load(from: makeURL()) { [weak self] result in
-            guard let self = self, let data = try? result.get() else {return}
-            self.presenter?.didFinishLoadingImageDataSuccessfuly(data)
-        }
-    }
-    
-    func didCancelLoadImageData() {
-        cancelTask()
-    }
-    
-    private func cancelTask() {
-        task?.cancel()
-        task = nil
-    }
-    
-    private func makeURL() -> URL {
-        let path = model.imagePath
-        return URL(string: "https://image.tmdb.org/t/p/w500/\(path)")!
-    }
-}
