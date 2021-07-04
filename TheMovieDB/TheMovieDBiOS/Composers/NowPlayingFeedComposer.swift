adac07afe831bc24ae84b1fb991a8bb920c6dae7
TungVu <tungvuduc2805@gmail.com>
2021-07-04 13:50:41 +0700

    migrate from `NowPlayingRefreshViewModel` to `NowPlayingRefreshPresenter`

--- TheMovieDB/TheMovieDBiOS/Composers/NowPlayingFeedComposer.swift
+++ TheMovieDB/TheMovieDBiOS/Composers/NowPlayingFeedComposer.swift
@@ -11,23 +11,15 @@ import UIKit
 
 public class NowPlayingFeedComposer {
     public static func viewControllerComposedWith(feedLoader: NowPlayingLoader, imageLoader: MovieImageDataLoader) -> NowPlayingFeedViewController {
-        let viewModel = NowPlayingRefreshViewModel(loader: MainQueueDispatchDecorator(decoratee: feedLoader))
-        let refreshController = NowPlayingRefreshController(viewModel: viewModel)
+        let adapter = NowPlayingRefreshRepresentationAdapter(loader: MainQueueDispatchDecorator(decoratee: feedLoader))
+        let refreshController = NowPlayingRefreshController(delegate: adapter)
         let viewController = NowPlayingFeedViewController(refreshController: refreshController)
-        
-        viewModel.onLoadFeed = adaptCellControllers(from: viewController,
-                                                           and: imageLoader)
+                
+        adapter.presenter = NowPlayingRefreshPresenter(loadingView: refreshController,
+                                                       feedView: FeedViewAdapter(viewController: viewController, imageLoader: imageLoader),
+                                                       errorView: viewController)
         
         return viewController
-    }
-    
-    private static func adaptCellControllers(from viewController: NowPlayingFeedViewController, and loader: MovieImageDataLoader) -> (NowPlayingFeed) -> Void{
-        {[weak viewController] feed in
-            viewController?.cellControllers = feed.items.map {
-                let viewModel = NowPlayingItemViewModel(model: $0, imageLoader: MainQueueDispatchDecorator(decoratee: loader), transformer: UIImage.init)
-                return NowPlayingItemController(viewModel: viewModel)
-            }
-        }
     }
 }
 
@@ -50,4 +42,44 @@ extension MainQueueDispatchDecorator: NowPlayingLoader where T == NowPlayingLoad
             }
         }
     }
+}
+
+private class FeedViewAdapter: FeedView {
+    private weak var viewController: NowPlayingFeedViewController?
+    private let imageLoader: MovieImageDataLoader
+    
+    init(viewController: NowPlayingFeedViewController, imageLoader: MovieImageDataLoader) {
+        self.viewController = viewController
+        self.imageLoader = imageLoader
+    }
+    
+    func display(_ viewModel: FeedViewModel) {
+        viewController?.cellControllers = viewModel.feed.items.map {
+            let viewModel = NowPlayingItemViewModel(model: $0, imageLoader: MainQueueDispatchDecorator(decoratee: imageLoader), transformer: UIImage.init)
+            return NowPlayingItemController(viewModel: viewModel)
+        }
+    }
+}
+
+private class NowPlayingRefreshRepresentationAdapter: NowPlayingRefreshDelegate {
+    private let loader: NowPlayingLoader
+    var presenter: NowPlayingRefreshPresenter?
+    
+    init(loader: NowPlayingLoader) {
+        self.loader = loader
+    }
+    
+    
+    func didRequestRefreshFeed() {
+        presenter?.didStartLoadingFeed()
+        loader.load(query: .init(page: 1)) {[weak self] result in
+            switch result {
+            case let .success(feed):
+                self?.presenter?.didFinishLoadingFeedSuccessfully(feed)
+            case let .failure(error):
+                self?.presenter?.didFinishLoadingFeedWithError(error)
+            }
+        }
+    }
+    
 }
---
+++ TheMovieDB/TheMovieDBiOS/View Models/NowPlayingRefreshPresenter.swift
@@ -0,0 +1,59 @@
+//
+//  NowPlayingRefreshPresenter.swift
+//  TheMovieDBiOS
+//
+//  Created by Tung Vu on 03/07/2021.
+//
+
+import Foundation
+import TheMovieDB
+
+struct FeedLoadingStateViewModel {
+    let isLoading: Bool
+}
+
+protocol FeedLoadingStateView: AnyObject {
+    func display(_ viewModel: FeedLoadingStateViewModel)
+}
+
+struct FeedViewModel {
+    let feed: NowPlayingFeed
+}
+
+protocol FeedView {
+    func display(_ viewModel: FeedViewModel)
+}
+
+struct ErrorViewModel {
+    let description: String
+}
+
+protocol ErrorView: AnyObject {
+    func display(_ viewModel: ErrorViewModel)
+}
+
+public final class NowPlayingRefreshPresenter {
+    private weak var loadingView: FeedLoadingStateView?
+    private let feedView: FeedView
+    private weak var errorView: ErrorView?
+    
+    init(loadingView: FeedLoadingStateView, feedView: FeedView, errorView: ErrorView) {
+        self.loadingView = loadingView
+        self.feedView = feedView
+        self.errorView = errorView
+    }
+
+    func didStartLoadingFeed() {
+        loadingView?.display(.init(isLoading: true))
+    }
+    
+    func didFinishLoadingFeedSuccessfully(_ feed: NowPlayingFeed) {
+        loadingView?.display(.init(isLoading: false))
+        feedView.display(.init(feed: feed))
+    }
+    
+    func didFinishLoadingFeedWithError(_ error: Error) {
+        loadingView?.display(.init(isLoading: false))
+        errorView?.display(.init(description: error.localizedDescription))
+    }
+}
